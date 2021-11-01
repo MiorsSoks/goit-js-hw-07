bb85b554d2658963f0ef491cab9de2a80f24ccbf
Myroslav Sokolov <mirossoks@gmail.com>
2021-11-01 15:29:25 +0200

    create gallery-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const items = galleryItems
+
+const galleryContainer = document.querySelector('.gallery')
+
+const markup = galleryItems
+  .map(
+    (el, index) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      data-index="${index}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`,
+  )
+  .join('');
+galleryContainer.innerHTML = markup;
+
+    
+const modalOpen = (e) => {
+    e.preventDefault()
+
+const instance = basicLightbox.create(`
+    <img alt = "${e.target.dataset.description}" src="${e.target.dataset.source}">
+`)
+
+instance.show()
+}
+
+galleryContainer.addEventListener('click', modalOpen)
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const items = galleryItems
+
+const galleryContainer = document.querySelector('.gallery')
+
+const markup = galleryItems
+  .map(
+    (el) =>
+      `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`,
+  )
+  .join('');
+galleryContainer.innerHTML = markup;
+
+
+var lightbox = new SimpleLightbox('.gallery a', { captions: true, captionsData:'alt', captionPosition: 'bottom', captionDelay: 250, });
+
+
+galleryContainer.addEventListener('click', lightbox)
